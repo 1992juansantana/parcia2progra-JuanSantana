4ba9c6e1d16d61a25dd4ac9c40a68cd4de8c51b2
juan santana <juan_92sjm@hotmail.com>
2024-12-03 19:56:34 -0300

    parcial 2

---
+++ GAME_OVER/personaje.py
@@ -0,0 +1,26 @@
+# import pygame
+# import random
+# from constantes import *
+# from main import *
+
+# class Personaje():
+#     def __init__(self, x, y):
+#         self.forma = pygame.Rect(0, 0, ANCHO_PERSONAJE, ALTO_PERSONAJE)#creacion de personaje, con pygame.rec se crea un rectangulo de personaje
+#         self.forma.center = (x, y)
+        
+#     def dibujar(self, interfaz):
+#         pygame.draw.rect(interfaz, COLOR_AMARILLO , self.forma)
+#         jugador = nave.get_rect()
+        
+        
+#     def movimiento(self, delta_x, delta_y):
+#         self.forma.x = self.forma.x + delta_x
+#         self.forma.y = self.forma.y + delta_y
+        
+    #  #Generar meteoritos
+    # def meteorito(self, x, y):
+    #     self.meteor = pygame.Rect(0, 0, ANCHO_METEORITO, ALTO_METEORITO)
+    #     self.meteor = (random.randint)
+    #     meteors = []
+    
+   
